dc95fa014465dc75b7d9e73adf55733bd4fe975b
Harrison Liang <liangharrison123@gmail.com>
2024-02-24 14:09:01 -0500

    shoe model complete

---
+++ backend/models/Shoe.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const shoeSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: "Category"
+    },
+    photoUrl: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Shoe', shoeSchema);
