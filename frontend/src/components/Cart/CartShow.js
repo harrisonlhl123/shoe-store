246ea3189e404c1dd1ebab24c1f3cc74c81f1cd9
Harrison Liang <liangharrison123@gmail.com>
2024-03-28 13:02:01 -0400

    show items showing

--- frontend/src/components/Cart/CartShow.js
+++ frontend/src/components/Cart/CartShow.js
@@ -1,8 +1,32 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { fetchCart } from "../../store/cart";
+// import { useSelector } from "react-redux/es/hooks/useSelector";
+
+
+// const CartShow = () => {
+//     const dispatch = useDispatch();
+//     const user = useSelector(state => state.session.user);
+
+//     useEffect(() => {
+//         dispatch(fetchCart(user._id))
+//     }, [])
+
+
+//     return (
+//         <h1>Cart Show page</h1>
+//     )
+// }
+
+// export default CartShow;
+
+
+
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchCart } from "../../store/cart";
 import { useSelector } from "react-redux/es/hooks/useSelector";
-
+import { fetchShoe, fetchShoes } from "../../store/shoes";
 
 const CartShow = () => {
     const dispatch = useDispatch();
@@ -10,12 +34,40 @@ const CartShow = () => {
 
     useEffect(() => {
         dispatch(fetchCart(user._id))
+    }, []);
+
+    const cart = useSelector(state => state.cart?.items)
+    
+    return (
+        <div>
+            <h1>Cart Show page</h1>
+            <div>
+                {cart && Object.values(cart).map(item => (
+                    <div key={item._id}>
+                        <h3>{item.quantity} x {item.size}</h3>
+                        <ShoeDetails shoeId={item.shoeId._id} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+const ShoeDetails = ({ shoeId }) => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchShoes());
     }, [])
 
+    const shoe = useSelector(state => state.shoes ? state.shoes[shoeId] : null)
 
     return (
-        <h1>Cart Show page</h1>
-    )
+        <div>
+            <img src={shoe?.photoUrl} alt={shoe?.name} />
+            <h4>{shoe?.name}</h4>
+            <p>Price: ${shoe?.price}</p>
+        </div>
+    );
 }
 
 export default CartShow;
