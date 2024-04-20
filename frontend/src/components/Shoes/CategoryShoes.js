11bded2c938673a211163c9e313bbb3d601d8142
Harrison Liang <liangharrison123@gmail.com>
2024-04-20 11:48:21 -0400

    shoes filtered by categories loaded

--- frontend/src/components/Shoes/CategoryShoes.js
+++ frontend/src/components/Shoes/CategoryShoes.js
@@ -1,6 +1,22 @@
+import { useDebugValue, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { fetchCategoryShoes } from "../../store/shoes";
+
 const CategoryShoes = () => {
+    const { categoryId } = useParams();
+    const dispatch = useDispatch()
+    const categoryShoes = useSelector((state) => state.shoes ? Object.values(state.shoes) : [])
+    useEffect(() => {
+        dispatch(fetchCategoryShoes(categoryId))
+    }, [])
+
     return (
-        <h1>Shoes by category</h1>
+        <>
+            {categoryShoes.map((shoe) => {
+                return shoe.name
+            })}
+        </>
     )
 }
 
