08d41ebef38a3f38bfed10950fce55b661ae5653
Harrison Liang <liangharrison123@gmail.com>
2024-04-20 12:54:12 -0400

    categories complete

--- frontend/src/components/Categories/OneCategory.js
+++ frontend/src/components/Categories/OneCategory.js
@@ -4,11 +4,9 @@ import './OneCategory.css';
 const OneCategory = ({category}) => {
     return (
         <>
-            <div id='one-category'>
-                <Link id='category-link' to={`/categories/${category._id}`}>
-                    <h3>{category.name}</h3>
-                </Link>
-            </div>
+            <Link id='category-link' to={`/categories/${category._id}`}>
+                <h3>{category.name}</h3>
+            </Link>
         </>
     )
 }
--- frontend/src/components/Shoes/CategoryShoes.js
+++ frontend/src/components/Shoes/CategoryShoes.js
@@ -2,21 +2,24 @@ import { useDebugValue, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { fetchCategoryShoes } from "../../store/shoes";
+import OneShoe from "./OneShoe";
+import './CategoryShoes.css';
 
 const CategoryShoes = () => {
     const { categoryId } = useParams();
     const dispatch = useDispatch()
     const categoryShoes = useSelector((state) => state.shoes ? Object.values(state.shoes) : [])
+
     useEffect(() => {
         dispatch(fetchCategoryShoes(categoryId))
     }, [])
 
     return (
-        <>
+        <div id="category-shoes">
             {categoryShoes.map((shoe) => {
-                return shoe.name
+                return <OneShoe key={shoe._id} shoe={shoe} />
             })}
-        </>
+        </div>
     )
 }
 
